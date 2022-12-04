67c47dff10fe1163ef8f03ed6840f05a422574ca
Andrey3691 <118386565+Andrey3691@users.noreply.github.com>
2022-12-04 16:23:09 +0300

    int gutnumber

---
+++ Program1.cs
@@ -0,0 +1,25 @@
+/*
+int CutNumber(int num)
+{
+    int ed = num % 10;
+    int sot = num / 100;
+
+    int result = sot * 10 + ed;
+    return result;
+}
+
+int randNum = new Random().Next(100, 1000);
+int newNum = CutNumber(randNum);
+
+Console.WriteLine($"New version of a nuber {randNum} is {newNum}");
+*/
+
+// Случайное число из отрезка от 10 до 99 и показывает наибольшую цифру числа.
+
+int GutNumber(int num)
+{
+    int ed = num % 10;
+    int des = num / 10;
+
+    int result = des >= ed
+}
