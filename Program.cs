966039f85d24abc895267b885942fb8b051faf9e
Andrey3691 <118386565+Andrey3691@users.noreply.github.com>
2022-12-07 05:52:19 +0300

    Исправление программы четное число ол 1 до n

--- Program.cs
+++ Program.cs
@@ -108,16 +108,15 @@ else
 */
 // Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 /*
-Console.Write("Write number 1: ");
-int n = int.Parse(Console.ReadLine()!);
-Console.Write("Write number 2: ");
-int m = int.Parse(Console.ReadLine()!);
-for (int i = n; i <= m; i++)
+Console.Write("Введите число n: ");
+int n;
+n = Convert.ToInt32(Console.ReadLine());
+for (int i = n; i <= n; i++)
 {
-  if (i % 2 == 0)
-  {
-    Console.Write(i + " ");
-  }
+	if (i % 2 == 0)
+	{
+		Console.Write(i + " ");
+	}
 }
 Console.ReadKey();
 */
