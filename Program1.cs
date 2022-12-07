05275c67d26dc101f8f431bdd814367d8ef44c7d
Andrey3691 <118386565+Andrey3691@users.noreply.github.com>
2022-12-07 14:12:59 +0300

    трёхзначное число

--- Program1.cs
+++ Program1.cs
@@ -75,8 +75,13 @@ bool result = Kvadrat(num1, num2);
 Console.WriteLine(($"Результат проверки: {result}"));
 */
 //  Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+/*
 
-
+Console.Write("Введи трёхзначное число: ");
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(threeDigitNumber);
+Console.WriteLine("вторая цифра этого числа -> "+stringNumber[1]);
+*/
 
 //  Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
