ac5d2394772afd68aad5918649466823486b81b0
Andrey3691 <118386565+Andrey3691@users.noreply.github.com>
2022-12-07 13:10:20 +0300

    Программа заданых координат

--- Program2.cs
+++ Program2.cs
@@ -1,6 +1,6 @@
 // Написать программу, определение координат
-
-int FindQadrand (double x, double y)
+/*
+int FindQuadrand (double x, double y)
 {
     if(x > 0 && y > 0) return 1;
     if(x < 0 && y > 0) return 2;
@@ -12,6 +12,22 @@ Console.Write("Input an x-coordinate: ");
 double xA = Convert.ToDouble(Console.ReadLine());
 Console.Write("Input an y-coordinate: ");
 double yA = Convert.ToDouble(Console.ReadLine());
-int qadNum = FindQadra(xA, yA);
+int qadNum = FindQuadrad(xA, yA);
 Console.WriteLine($"The point A {xA},{yA} is on the {qadNum} qadRand");
+*/
 
+
+
+int GetCoordinates(int quadrant)
+{
+    if(quadrant == 1) Console.WriteLine("x>0, y>0");
+    else if(quadrant == 2) Console.WriteLine("x<0, y>0");
+    else if(quadrant == 3) Console.WriteLine("x<0, y<0");
+    else if(quadrant == 4) Console.WriteLine("x>0, y<0");
+    else Console.WriteLine("There is on such quadrant");
+}
+
+Console.WriteLine("Input number is the quadrant: ");
+int quadrantPoits = Convert.ToInt32(Console.ReadLine());
+
+GetCoordinates(quadrantPoits);
