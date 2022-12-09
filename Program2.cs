5afca2b82522a94d4d6bb31dd6e356cdbf1a6c3b
Andrey3691 <118386565+Andrey3691@users.noreply.github.com>
2022-12-09 09:31:29 +0300

    Праграмма координат 3

--- Program2.cs
+++ Program2.cs
@@ -33,6 +33,7 @@ int quadrantPoits = Convert.ToInt32(Console.ReadLine());
 GetCoordinates(quadrantPoits);
 */
 
+/*
 void GetCoordinates(int quadrant)
 {
     if(quadrant == 1) Console.WriteLine("x>0, y>0");
@@ -45,3 +46,27 @@ Console.WriteLine("Input number is the quadrant: ");
 int quadrantPoits = Convert.ToInt32(Console.ReadLine());
 
 GetCoordinates(quadrantPoits);
+*/
+
+
+double Distance(double xA, double yA, double xB, double yB)
+{
+    double Lingh = Math.Sqrt(Math.Pow(xB - xA, 2) + Math.Pow(yB - yA, 2));
+    return Math.Round(Lingh, 2);
+}
+Console.WriteLine("Input an x-coordonate of point A: ");
+double xA = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Input an y-coordonate of point A: ");
+double yA = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Input an x-coordonate of point B: ");
+double xB = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Input an y-coordonate of point B: ");
+double yB = Convert.ToDouble(Console.ReadLine());
+
+double distanceBetweenAB = Distance(xA, xB, yA, yB);
+
+Console.WriteLine("Distance Between points A and B is {distanceBetweenAB}.");
+
