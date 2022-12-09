f01d71fb2f944d7655ab902f66a470850ddb6e26
Andrey3691 <118386565+Andrey3691@users.noreply.github.com>
2022-12-09 08:22:07 +0300

    Программа координат 2

--- Program2.cs
+++ Program2.cs
@@ -32,3 +32,16 @@ int quadrantPoits = Convert.ToInt32(Console.ReadLine());
 
 GetCoordinates(quadrantPoits);
 */
+
+void GetCoordinates(int quadrant)
+{
+    if(quadrant == 1) Console.WriteLine("x>0, y>0");
+    else if(quadrant == 2) Console.WriteLine("x<0, y>0");
+    else if(quadrant == 3) Console.WriteLine("x<0, y<0");
+    else if(quadrant == 4) Console.WriteLine("x>0, y<0");
+    else Console.WriteLine("There is on such quadrant");
+}
+Console.WriteLine("Input number is the quadrant: ");
+int quadrantPoits = Convert.ToInt32(Console.ReadLine());
+
+GetCoordinates(quadrantPoits);
