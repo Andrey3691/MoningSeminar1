852f22f021028404dd263b20891d73a7d1483189
Andrey3691 <118386565+Andrey3691@users.noreply.github.com>
2022-12-10 04:29:01 +0300

    палиндром

--- Program2.cs
+++ Program2.cs
@@ -91,6 +91,25 @@ Squares(num);
 
 // Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
 
+/*
+Console.Write("Введите число: ");
+string? number = Console.ReadLine();
+
+void CheckingNumber(string number)
+{
+  if (number[0]==number[4] || number[1]==number[3])
+  {
+    Console.WriteLine($"Ваше число: {number} - палиндром.");
+  }
+  else Console.WriteLine($"Ваше число: {number} - НЕ палиндром.");
+}
+
+if (number!.Length == 5)
+{
+  CheckingNumber(number);
+}
+else Console.WriteLine($"Введи правильное число");
+*/
 
 // Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 
