327b4f35eae4408ed755fea8abf0bef25078c341
Andrey3691 <118386565+Andrey3691@users.noreply.github.com>
2022-12-14 12:18:04 +0300

    выдаёт сумму цифр в числе

--- Program3.cs
+++ Program3.cs
@@ -136,7 +136,6 @@ int Exponentiation(int numberA, int numberB)
     // int result = Math.Pow(numberA, numberB);
   return result;
 }
-
 Console.Write("Введите число A: ");
 int numberA = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите число B: ");
@@ -144,4 +143,25 @@ int numberB = Convert.ToInt32(Console.ReadLine());
 
 int exponentiation = Exponentiation(numberA, numberB);
 Console.WriteLine("Ответ: " + exponentiation);
+*/
+// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+/*
+Console.Write("Введите число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+  int SumNumber(int numberN)
+  {
+    int counter = Convert.ToString(numberN).Length;
+    int advance = 0;
+    int result = 0;
+    for (int i = 0; i < counter; i++)
+    {
+      advance = numberN - numberN % 10;
+      result = result + (numberN - advance);
+      numberN = numberN / 10;
+    }
+    return result;
+  }
+int sumNumber = SumNumber(numberN);
+Console.WriteLine("Сумма цифр в числе: " + sumNumber);
 */
